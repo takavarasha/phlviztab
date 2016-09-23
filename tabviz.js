0295e65af6a38556e173f0d4dbd850b12fdc3ce2
Godfrey Takavarasha <takavarasha@users.noreply.github.com>
2016-09-22 23:14:31 -0400

    initial upload

---
+++ tabviz.js
@@ -0,0 +1,20 @@
+function openTab(e, tabName) {
+    // Declare all variables
+    var i, tabContent, tabLinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabContent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tabLinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    e.currentTarget.className += " active";
+}
